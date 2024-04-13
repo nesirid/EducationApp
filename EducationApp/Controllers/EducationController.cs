3fec3b301280bf639f43f8421e4c4118c66151dd
nesirid <nasirid@code.edu.az>
2024-04-13 18:57:18 +0400

    education controller methods is ready

--- EducationApp/Controllers/EducationController.cs
+++ EducationApp/Controllers/EducationController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Domain.Models;
+using System.Xml.Linq;
 
 namespace EducationApp.Controllers
 {
@@ -28,7 +29,7 @@ namespace EducationApp.Controllers
             await _educationService.Create(new Education { Name = eduName, Color = eduColor, CreatedTime = DateTime.UtcNow });
 
         }
-        public async Task Delete() 
+        public async Task Delete()
         {
             Console.WriteLine("Enter Education Id");
             int id = Convert.ToInt32(Console.ReadLine());
@@ -42,6 +43,82 @@ namespace EducationApp.Controllers
                 Console.WriteLine(ex.Message);
             }
         }
+        public async Task GetAll()
+        {
+            var datas = await _educationService.GetAll();
+            foreach (var item in datas)
+            {
+                string data = $"Name : {item.Name}, Color : {item.Color}, Created Date : {item.CreatedTime}";
+                Console.WriteLine(data);
+            }
+        }
+        public async Task GetById()
+        {
+            Console.WriteLine("Write Education Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                var data = await _educationService.GetById(id);
+                string result = $"Name : {data.Name}, Color : {data.Color}, Created Date : {data.CreatedTime}";
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public async Task GetAllWithGroups()
+        {
+            await _educationService.GetAllWithGroups();
+        }
+        public async Task Search()
+        {
+            Console.WriteLine("Enter input to search");
+            string input = Console.ReadLine();
 
+            var datas = await _educationService.Search(input);
+
+            foreach (var item in datas)
+            {
+                string data = $"Name : {item.Name}, Color :  {item.Color} , Created Date :  {item.CreatedTime}";
+                Console.WriteLine(data);
+            }
+        }
+        public async Task SortWithCreatedDate()
+        {
+            var datas = await _educationService.SortWithCreatedDate();
+            foreach (var item in datas)
+            {
+                string data = $"Name : {item.Name}, Color : {item.Color}, Created Date : {item.CreatedTime}";
+                Console.WriteLine(data);
+            }
+        }
+        public async Task Update()
+        {
+            Console.WriteLine("Enter Education Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                var data = await _educationService.GetById(id);
+                Console.WriteLine("Enter Name:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter Color:");
+                //string capStr=Console.ReadLine();
+                string color = Console.ReadLine();
+
+                var updatedEducation = new Education
+                {
+                    Id = data.Id,
+                    Name = name,
+                    Color = color
+                };
+                await _educationService.Update(updatedEducation);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
